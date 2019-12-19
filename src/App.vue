9253c46f1021eb6c6d5b6489262190d02c3e375a
shabai517 <shabai517@163.com>
2019-12-19 21:27:51 +0000

    refine global css

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #fff;
-  background: #f8f8f8;
+  color: #565656;
+  /*background: #f8f8f8;*/
   /*min-width: 600px;*/
   font-size: 14px;
   overflow: hidden;
